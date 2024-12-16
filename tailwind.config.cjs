f0538c573d8067e7d34199f78576e702c888ceb2
Shreyan1729 <shreyandeyrudra@gmail.com>
2024-12-16 22:32:34 +0600

    first commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
-  mode: "jit",
+export default {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Include all relevant file types in your project
+  ],
+  mode: "jit", // Optional: JIT mode for improved performance
   theme: {
     extend: {
       colors: {
